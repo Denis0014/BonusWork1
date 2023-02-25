5f391b30bd322443c99768046c63f83302da1563
Denis <109104246+Denis0014@users.noreply.github.com>
2023-02-25 19:26:20 +0300

    Задание 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,9 +9,9 @@ namespace BonusWork1
         /// Типы операций в методе Action 
         /// </summary>
         /// <remarks>
-        /// <br>Sum: преобразовать все параметры к целым числам и вычислить их сумму.</br>
-        /// <br>Average: преобразовать все параметры к вещественным числам и вычислить их среднее арифметическое.</br>
-        /// <br>Multiply: Преобразовать все параметры к long и перемножить их.</br>
+        /// Sum: преобразовать все параметры к целым числам и вычислить их сумму.<br></br>
+        /// Average: преобразовать все параметры к вещественным числам и вычислить их среднее арифметическое.<br></br>
+        /// Multiply: Преобразовать все параметры к long и перемножить их.
         /// </remarks>
         enum ActionType { Sum, Average, Multiply };
         /// <summary>
@@ -43,7 +43,15 @@ namespace BonusWork1
                     throw new ArgumentException("Action: Неверный тип оперции");
             }
         }
-
+        /// <summary>
+        /// Возвращает, является ли строка счастливой
+        /// </summary>
+        /// <remarks>
+        /// Будем называть строку счастливой, если, просуммировав коды символов,<br></br>
+        /// составляющих эту строку, получается число, оканчивающееся на цифру 7
+        /// </remarks>
+        /// <param name="s"></param>
+        /// <returns></returns>
         static bool StringIsHappy(string s) => s.ToCharArray().Sum(x => x) % 10 == 7;
 
         static void Main(string[] args)
